e3e96e37fc58300d0760b06cae63543e555fadff
Adib Mosharrof <adib.mosharrof@gmail.com>
2019-06-04 22:00:35 -0700

    working version

--- cobrac.py
+++ cobrac.py
@@ -1,25 +1,32 @@
 '''
 CIS 510: Multiagent Systems
-HW1
+HW3
 Adib Mosharrof
-
-Note:
-Trevor Bergstorm and I solved this problem together, but then we each refactored
-it in our own style. So there might be some similarities in our codebase.
 
 max vDefender
     s.t.
-    z1+z2+z3 ... zn = 1 --- number of targets
-    x1+x2+x3 ....xn <=2 --- coverage probability of each target, 
+    r1 -> z1+z2+z3 ... zn = 1 --- number of targets
+    r2 -> q1+q2+q3 ... qn >= 1 
+    r3 -> x1+x2+x3 ....xn <=2 --- coverage probability of each target, 
                             sum should be less than number of resources
     
-    defender strategy constraint
-    vDefender + (uDu - uDc)xi + Mzi M= M + uDu
-
-    attacker strategy constraint
-    vAttacker + (uAu - uAc)xi >= uAu
-       attacker utility constraint 
-    vAttacker + (uAu - uAc)xi + Mzi  <= M + uAu 
+    r4 to r8
+    Find attacker optimal targets
+    vA + (UAU - UAC)xt' >= UAU
+    r9 to r13
+    vA + (UAU - UAC)xt' + M zt <= M + UAU
+    
+    Find E-optimal targets
+    r14 to r18
+    vA + (UAU - UAC)xt' + Eqt >= E + UAU
+    r19 to r23
+    vA + (UAU - UAC)xt' + Mqt < E+M+UAU
+    
+    Perceived strategy
+    x't + (Alpha -1)xt = Alpha/NumTargets
+    
+    Defender worst case utility
+    vD + (UDU - UDC)xt + Mqt < M+ UDU
 
 '''
 
@@ -27,21 +34,26 @@ import sys
 import csv
 import cplex
 import copy
+from decimal import *
 
 numOfTargets = None
 numOfResources = None
 M = 1000
+alpha = None
+E = None
 
 payoffMatrix = []
 
 def loadData():
-    global numOfTargets
-    global numOfResources
+    global numOfTargets,numOfResources,alpha, E
+    
     with open('param.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
         for row in spamreader:
             numOfTargets = int(row[0])
             numOfResources = int(row[1])
+            alpha = float(row[2])
+            E = float(row[3])
 
     with open('payoff.csv', newline='') as csvfile:
         spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
@@ -52,11 +64,12 @@ def loadData():
             payoffMatrix.append(row)
 
 def getObjective():
+    #updated
     #objective function is to maximize vDefender, so only vDefender coefficient will be 1,
     #rest will be 0
     objective = []
-    #making coefficients of all z and x 0
-    for i in range(numOfTargets*2):
+    #making coefficients of all z, q, x and x' 0
+    for i in range(numOfTargets*4):
         objective.append(0.0)
 
     #vDefender is 1
@@ -66,10 +79,11 @@ def getObjective():
     return objective
 
 def getUpperBounds():
+    #updated
     upperBounds = []
 
-    #upper bound for z and x is 1
-    for i in range(numOfTargets*2):
+    #upper bound for z,q, x and x' is 1
+    for i in range(numOfTargets*4):
         upperBounds.append(1.0)
 
     #upper bounds for vDefender and vAttacker is infinity
@@ -78,9 +92,10 @@ def getUpperBounds():
     return upperBounds
 
 def getLowerBounds():
+    #updated
     lowerBounds = []
-    #upper bound for z and x is 1
-    for i in range(numOfTargets*2):
+    #upper bound for z, q, x and x' is 1
+    for i in range(numOfTargets*4):
         lowerBounds.append(0.0)
 
     #upper bounds for vDefender and vAttacker is infinity
@@ -89,11 +104,16 @@ def getLowerBounds():
     return lowerBounds
 
 def getCTypes():
+    #updated
     cTypes = ''
     
     #z is an integer {0,1} which represents whether a target will be attacked or not
     cTypes += numOfTargets*'I'
+    #q is an integer {0,1}, which represents the targets within E of optimal target
+    cTypes += numOfTargets*'I'
     #x is the probabilty distribution [0,1]
+    cTypes += numOfTargets*'C'
+    #x' is the probabilty distribution [0,1]
     cTypes += numOfTargets*'C'
     
     #continues values for vDefender and vAttacker
@@ -101,62 +121,53 @@ def getCTypes():
     return cTypes;
 
 def getColumnNames():
+    #updated
     columnNames = []
+    names = ['z', 'q', 'x', 'x\'']
+    for name in names:
+        for i in range(numOfTargets):
+            columnNames.append(name+str(i+1))
 
-    for i in range(numOfTargets):
-        columnNames.append('z'+str(i+1))
-
-    for i in range(numOfTargets):
-        columnNames.append('x'+str(i+1))
     
     columnNames.append('vDefender')    
     columnNames.append('vAttacker')
     return columnNames
 
 def getRightHandSide():
-    '''
-    r1 -> z1+z2+z3 ... zn = 1 --- number of targets
-    r2 -> x1+x2+x3 ....xn <=2 --- coverage probability of each target, 
-                            sum should be less than number of resources
-    
-    defender strategy constraint
-    vDefender + (uDu - uDc)xi + Mzi M= M + uDu
-    r3 to r(3 +numOfTargets)  M + uDu
-
-    attacker strategy constraint
-    vAttacker + (uAu - uAc)xi >= uAu
-    r(4+numOfTargets) to r(4 +numOfTargets*2)  uAu
-
-       attacker utility constraint 
-    vAttacker + (uAu - uAc)xi + Mzi  <= M + uAu 
-    r(5+numOfTargets*2) to r(5 +numOfTargets*3)  M + uAu 
-    '''
-
     rhs = []
 
     #r1
     rhs.append(1.0)
     #r2
+    rhs.append(1.0)
+    #r3
     rhs.append(numOfResources)
 
-    #defender strategy
+    #attacker optimal target
+    #utility of AttackerUncovered is at index 2
     for i in range(numOfTargets):
-        #utility of DefenderUncovered is at index 1
-        rhs.append(M+ payoffMatrix[i][1])
-
-    #attacker strategy
-    for i in range(numOfTargets):
-        #utility of DefenderUncovered is at index 2
         rhs.append(payoffMatrix[i][2])
-
-    #attacker utility
     for i in range(numOfTargets):
-        rhs.append(M+ payoffMatrix[i][2])
+        rhs.append(M+payoffMatrix[i][2])
+        
+    #attacker E-optimal target
+    for i in range(numOfTargets):
+        rhs.append(E+payoffMatrix[i][2])
+    for i in range(numOfTargets):
+        rhs.append(E+M+payoffMatrix[i][2])
+        
+    #perceived strategy
+    for i in range(numOfTargets):
+        rhs.append(alpha/numOfTargets)
+        
+    #defender worst case utility
+    for i in range(numOfTargets):
+        rhs.append(M+payoffMatrix[i][1])    
 
     return rhs
 
 def getRowNames():
-    numOfContstraints = numOfTargets*3 + 2
+    numOfContstraints = numOfTargets*6 + 3
     rowNames = []
     for i in range(numOfContstraints):
         rowNames.append("r"+str(i+1))
@@ -165,19 +176,23 @@ def getRowNames():
 def getSense():
     #sense is used to denote inequality of each constraint, 
     sense = ''
-
     #sum of z has an equal constraint
     sense += 'E'
+    #sum of q is greater than
+    sense += 'G'
     #sum of x has less than constraint
     sense += 'L'
-
-    #defender utility has less than constraint
+    #attacker optimal target
+    sense += numOfTargets *'G'
     sense += numOfTargets*'L'
-    #attacker strategy has greater than constraint
-    sense += numOfTargets*'G'
-    #attacker utility has less than constraint
+    #attacker E-optimal target
+    sense += numOfTargets *'G'
     sense += numOfTargets*'L'
-
+    #perceived strategy
+    sense += numOfTargets*'E'
+    #defender worst case utility
+    sense += numOfTargets*'L'
+    
     return sense;
 
 def getRows():
@@ -197,45 +212,71 @@ def getRows():
     #for both the coefficient is 1
     zNames = []
     zCoefficients = []
+    qNames = []
+    qCoefficients = []
     xNames = []
     xCoefficients = []
 
     for i in range(numOfTargets):
         zNames.append("z"+ str(i+1))
         zCoefficients.append(1.0)
-
+        
+        qNames.append("q"+ str(i+1))
+        qCoefficients.append(1.0)
+        
         xNames.append("x"+ str(i+1))
         xCoefficients.append(1.0)
 
     rows.append([zNames, zCoefficients])
+    rows.append([qNames, qCoefficients])
     rows.append([xNames, xCoefficients])
-
 
     variables = []
     coefficients = []
 
-    #defender strategy constraint
-    #vDefender + (uDu - uDc)xi + Mzi M= M + uDu
+    #attacker optimal target
+    #vA + (UAU - UAC)xt' >= UAU
     for i in range(numOfTargets):
-        variables = ['vDefender', 'x'+str(i+1), 'z'+str(i+1)]
-        coefficients = [1.0, payoffMatrix[i][1]- payoffMatrix[i][0], M]
+        variables = ['vAttacker', "x'"+str(i+1)]
+        coefficients = [1.0, payoffMatrix[i][2]- payoffMatrix[i][3]]
         addDataToRows(rows, variables, coefficients)
 
 
-    #attacker strategy
-    #vAttacker + (uAu - uAc)xi >= uAu
+    #attacker optimal target
+    #vA + (UAU - UAC)xt' + M zt <= M + UAU
     for i in range(numOfTargets):
-        variables = ['vAttacker', "x"+str(i+1)]
-        coefficients = [1.0, payoffMatrix[i][2] - payoffMatrix[i][3]]
+        variables = ['vAttacker', "x'"+str(i+1), "z"+str(i+1)]
+        coefficients = [1.0, payoffMatrix[i][2] - payoffMatrix[i][3], M]
+        addDataToRows(rows, variables, coefficients)
+    
+    #attacker E-optimal target
+    #vA + (UAU - UAC)xt' + Eqt >= E + UAU
+    for i in range(numOfTargets):
+        variables = ['vAttacker', "x'"+str(i+1), "q"+str(i+1)]
+        coefficients = [1.0, payoffMatrix[i][2]- payoffMatrix[i][3], E]
         addDataToRows(rows, variables, coefficients)
 
-    #attacker utility
-    #vAttacker + (uAu - uAc)xi + Mzi  <= M + uAu 
+
+    #attacker E-optimal target
+    #vA + (UAU - UAC)xt' + Mqt < E+M+UAU
     for i in range(numOfTargets):
-        variables = ['vAttacker', "x"+str(i+1), "z"+str(i+1)]
+        variables = ['vAttacker', "x'"+str(i+1), "q"+str(i+1)]
         coefficients = [1.0, payoffMatrix[i][2] - payoffMatrix[i][3], M]
         addDataToRows(rows, variables, coefficients)
 
+    #perceived strategy
+    #x't + (Alpha -1)xt = Alpha/NumTargets
+    for i in range(numOfTargets):
+        variables = ["x'"+str(i+1), "x"+str(i+1)]
+        coefficients = [1.0, alpha-1]
+        addDataToRows(rows, variables, coefficients)
+        
+    #Defender worst case utility
+    #vD + (UDU - UDC)xt + Mqt < M+ UDU
+    for i in range(numOfTargets):
+        variables = ['vDefender', "x"+str(i+1), "q"+str(i+1)]
+        coefficients = [1.0, payoffMatrix[i][1] - payoffMatrix[i][0], M]
+        addDataToRows(rows, variables, coefficients)
     return rows
 
 def addDataToRows(rows, variables, coefficients):    
@@ -248,7 +289,7 @@ def writeToFile(values):
             writer = csv.writer(outputFile, delimiter=',')
 
             for i in range(numOfTargets):
-                writer.writerow( [str(i + 1), str(values[i + numOfTargets])])
+                writer.writerow( [str(i + 1), str(values[i + numOfTargets*2])])
 
 def runCobrac():
     
